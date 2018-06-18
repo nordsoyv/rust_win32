4055a9909aeea69581dd8ee15d50b1b51096f91d
Øyvind Nordsveen <oyvind.nordsveen@confirmit.com>
2018-06-18 08:41:59 +0200

    -

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,227 @@
+// Let's put this so that it won't open console
+#![windows_subsystem = "windows"]
+
+#[cfg(windows)]
+extern crate winapi;
+extern crate libc;
+// https://docs.rs/winapi/*/x86_64-pc-windows-msvc/winapi/um/libloaderapi/index.html?search=winuser
+
+use std::ffi::OsStr;
+use std::os::windows::ffi::OsStrExt;
+use std::iter::once;
+use std::mem;
+use std::ptr::null_mut;
+use std::io::Error;
+
+use libc::c_int;
+
+use self::winapi::shared::windef::{
+    HWND,
+    RECT,
+    LPRECT,
+    HDC,
+    HMENU,
+};
+use self::winapi::shared::minwindef::{
+    LPARAM,
+    WPARAM,
+    UINT,
+    LRESULT,
+    HINSTANCE,
+    DWORD,
+};
+
+use self::winapi::um::libloaderapi::GetModuleHandleW;
+use self::winapi::um::winuser::{
+    DefWindowProcW,
+    RegisterClassW,
+    CreateWindowExW,
+    TranslateMessage,
+    DispatchMessageW,
+    GetMessageW,
+    PostQuitMessage,
+    GetWindowLongW,
+    BeginPaint,
+    GetClientRect,
+    DrawTextW,
+    EndPaint,
+};
+use self::winapi::um::winuser::{
+    MSG,
+    WNDCLASSW,
+    CS_OWNDC,
+    CS_HREDRAW,
+    CS_VREDRAW,
+    CW_USEDEFAULT,
+    WS_OVERLAPPEDWINDOW,
+    WS_VISIBLE,
+    LPPAINTSTRUCT,
+    DT_CENTER,
+    DT_VCENTER,
+    DT_SINGLELINE,
+    BS_DEFPUSHBUTTON,
+    WS_CHILD,
+    GWL_HINSTANCE,
+    PAINTSTRUCT,
+    WM_PAINT,
+    WM_DESTROY,
+    WM_CREATE,
+};
+
+
+// ----------------------------------------------------
+
+// We have to encode text to wide format for Windows
+#[cfg(windows)]
+fn win32_string(value: &str) -> *const u16 {
+    let v: Vec<u16> = OsStr::new(value).encode_wide().chain(once(0)).collect();
+    return v.as_ptr();
+}
+
+fn create_handle(dw_ex_style: DWORD, lp_class_name: &str, lp_window_name: &str, dw_style: DWORD, x: c_int,
+                 y: c_int, n_width: c_int, n_height: c_int, hwnd_parent: HWND, handle: Handle,
+                 hInstance: HINSTANCE) -> HWND {
+    unsafe {
+        return CreateWindowExW(dw_ex_style, win32_string(lp_class_name), win32_string(lp_window_name), dw_style,
+                               x, y, n_width, n_height, hwnd_parent, handle.value(), hInstance, std::ptr::null_mut());
+    }
+}
+
+// Window struct
+#[cfg(windows)]
+struct Window {
+    handle: HWND,
+}
+
+enum Handle {
+    Button,
+    Window,
+}
+
+impl Handle {
+    fn value(&self) -> HMENU {
+        match *self {
+            Handle::Button => 1001 as HMENU,
+            Handle::Window => 0 as HMENU,
+        }
+    }
+}
+
+// window Proc
+pub unsafe extern "system" fn window_proc(hwnd: HWND,
+                                          msg: UINT, w_param: WPARAM, l_param: LPARAM) -> LRESULT {
+    let hdc: HDC;
+    let lp_paint_struct: LPPAINTSTRUCT = libc::malloc(mem::size_of::<PAINTSTRUCT>() as libc::size_t) as *mut PAINTSTRUCT;
+    let lp_rect: LPRECT = libc::malloc(mem::size_of::<RECT>() as libc::size_t) as *mut RECT;
+    let button_dont_press_me: HWND;
+    let button_press_me: HWND;
+
+
+    match msg {
+        WM_CREATE => {
+            let h_instance = GetWindowLongW(hwnd, GWL_HINSTANCE) as HINSTANCE;
+            button_dont_press_me = create_handle(0, "Button", "Don't press me",
+                                              WS_CHILD | WS_VISIBLE | BS_DEFPUSHBUTTON, 100, 200, 50, 20,
+                                              hwnd, Handle::Button, h_instance);
+            button_press_me = create_handle(0, "Button", "Press me",
+                                          WS_CHILD | WS_VISIBLE | BS_DEFPUSHBUTTON, 200, 200, 50, 20,
+                                          hwnd, Handle::Button, h_instance);
+        }
+        WM_PAINT => {
+            hdc = BeginPaint(hwnd, lp_paint_struct);
+            GetClientRect(hwnd, lp_rect);
+            DrawTextW(hdc, win32_string("Done with pride and prejudice by Culeva Alex"), -1, lp_rect, DT_SINGLELINE | DT_CENTER | DT_VCENTER);
+            EndPaint(hwnd, lp_paint_struct);
+        }
+        WM_DESTROY => {
+            PostQuitMessage(0);
+        }
+        _ => {
+            return DefWindowProcW(hwnd, msg, w_param, l_param);
+        }
+    }
+
+    return 0;
+}
+
+// Create window function
+#[cfg(windows)]
+fn create_window(name: &str, title: &str) -> Result<Window, Error> {
+    let name = win32_string(name);
+    let title = win32_string(title);
+
+    unsafe {
+
+        // Create handle instance that will call GetModuleHandleW, which grabs the instance handle of WNDCLASSW (check third parameter)
+        let h_instance = GetModuleHandleW(null_mut());
+
+        // Create "class" for window, using WNDCLASSW struct (different from Window our struct)
+        let wnd_class = WNDCLASSW {
+            style: CS_OWNDC | CS_HREDRAW | CS_VREDRAW,        // Style
+            lpfnWndProc: Some(window_proc),            // The callbackfunction for any window event that can occur in our window!!! Here you could react to events like WM_SIZE or WM_QUIT.
+            hInstance: h_instance,                            // The instance handle for our application which we can retrieve by calling GetModuleHandleW.
+            lpszClassName: name,                    // Our class name which needs to be a UTF-16 string (defined earlier before unsafe). as_ptr() (Rust's own function) returns a raw pointer to the slice's buffer
+            cbClsExtra: 0,
+            cbWndExtra: 0,
+            hIcon: null_mut(),
+            hCursor: null_mut(),
+            hbrBackground: null_mut(),
+            lpszMenuName: null_mut(),
+        };
+
+        // We have to register this class for Windows to use
+        RegisterClassW(&wnd_class);
+
+        // More info: https://msdn.microsoft.com/en-us/library/windows/desktop/ms632680(v=vs.85).aspx
+        // Create a window based on registered class
+        let handle = CreateWindowExW(
+            0,                                    // dwExStyle
+            name,                        // lpClassName, name of the class that we want to use for this window, which will be the same that we have registered before.
+            title,                        // lpWindowName
+            WS_OVERLAPPEDWINDOW | WS_VISIBLE,    // dwStyle
+            CW_USEDEFAULT,                        // Int x
+            CW_USEDEFAULT,                        // Int y
+            CW_USEDEFAULT,                        // Int nWidth
+            CW_USEDEFAULT,                        // Int nHeight
+            null_mut(),                            // hWndParent
+            null_mut(),                            // hMenu
+            h_instance,                            // hInstance
+            null_mut());                        // lpParam
+
+        if handle.is_null() {
+            Err(Error::last_os_error())
+        } else {
+            Ok(Window { handle })
+        }
+    }
+}
+
+#[cfg(windows)]
+// Create message handling function with which to link to hook window to Windows messaging system
+// More info: https://msdn.microsoft.com/en-us/library/windows/desktop/ms644927(v=vs.85).aspx
+fn handle_message(window: &mut Window) -> bool {
+    unsafe {
+        let mut message: MSG = mem::uninitialized();
+
+        // Get message from message queue with GetMessageW
+        if GetMessageW(&mut message as *mut MSG, window.handle, 0, 0) > 0 {
+            TranslateMessage(&message as *const MSG); // Translate message into something meaningful with TranslateMessage
+            DispatchMessageW(&message as *const MSG); // Dispatch message with DispatchMessageW
+
+            true
+        } else {
+            false
+        }
+    }
+}
+
+#[cfg(windows)]
 fn main() {
-    println!("Hello, world!");
+    let mut window = create_window("my_window", "Portfolio manager pro").unwrap();
+
+    loop {
+        if !handle_message(&mut window) {
+            break;
+        }
+    }
 }
