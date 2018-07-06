7aa3ab9067a914b51ce733caefdaf744588cdd92
Øyvind Nordsveen <oyvind.nordsveen@confirmit.com>
2018-07-06 22:28:14 +0200

    memory stuff

--- src/main.rs
+++ src/main.rs
@@ -20,172 +20,240 @@ use std::ptr::null_mut;
 use std::io::Error;
 
 use self::winapi::shared::windef::{
-    HWND,
-    RECT,
-    LPRECT,
-    HDC,
+  HWND,
+  RECT,
+  LPRECT,
+  HDC,
 };
 use self::winapi::shared::minwindef::{
-    LPARAM,
-    WPARAM,
-    UINT,
-    LRESULT,
+  LPARAM,
+  WPARAM,
+  UINT,
+  LRESULT,
+  LPVOID,
 };
 
 use self::winapi::um::libloaderapi::GetModuleHandleW;
 use self::winapi::um::winuser::{
-    DefWindowProcW,
-    RegisterClassW,
-    CreateWindowExW,
-    TranslateMessage,
-    DispatchMessageW,
-    PeekMessageW,
-    PostQuitMessage,
-    BeginPaint,
-    GetClientRect,
-    DrawTextW,
-    EndPaint,
-    ShowWindow,
-    GetAsyncKeyState,
+  DefWindowProcW,
+  RegisterClassW,
+  CreateWindowExW,
+  TranslateMessage,
+  DispatchMessageW,
+  PeekMessageW,
+  PostQuitMessage,
+  BeginPaint,
+  GetClientRect,
+  DrawTextW,
+  EndPaint,
+  ShowWindow,
+  GetAsyncKeyState,
 };
 use self::winapi::um::winuser::{
-    MSG,
-    WNDCLASSW,
-    CS_OWNDC,
-    CS_HREDRAW,
-    CS_VREDRAW,
-    CW_USEDEFAULT,
-    WS_OVERLAPPEDWINDOW,
-    WS_VISIBLE,
-    LPPAINTSTRUCT,
-    DT_CENTER,
-    DT_VCENTER,
-    DT_SINGLELINE,
-    PAINTSTRUCT,
-    WM_PAINT,
-    WM_DESTROY,
-    WM_CREATE,
-    PM_REMOVE,
-    SW_HIDE,
-    VK_ESCAPE,
+  MSG,
+  WNDCLASSW,
+  CS_OWNDC,
+  CS_HREDRAW,
+  CS_VREDRAW,
+  CW_USEDEFAULT,
+  WS_OVERLAPPEDWINDOW,
+  WS_VISIBLE,
+  LPPAINTSTRUCT,
+  DT_CENTER,
+  DT_VCENTER,
+  DT_SINGLELINE,
+  PAINTSTRUCT,
+//    WM_PAINT,
+  WM_DESTROY,
+  WM_CREATE,
+  PM_REMOVE,
+  SW_HIDE,
+  VK_ESCAPE,
 };
 
+use self::winapi::um::wingdi::{
+  BITMAPINFO,
+  BITMAPINFOHEADER,
+  RGBQUAD,
+};
+use self::winapi::um::winnt::{
+  MEM_COMMIT,
+  PAGE_READWRITE,
+};
+
+use self::winapi::um::memoryapi::VirtualAlloc;
 use self::winapi::um::wincon::GetConsoleWindow;
 
 use std::time::Duration;
 use std::time::Instant;
+
+struct OffscreenBuffer {
+  info: BITMAPINFO,
+  width: i32,
+  height: i32,
+  pitch: i32,
+  memory: LPVOID,
+}
 
 // ----------------------------------------------------
 
 // We have to encode text to wide format for Windows
 #[cfg(windows)]
 fn win32_string(value: &str) -> *const u16 {
-    let v: Vec<u16> = OsStr::new(value).encode_wide().chain(once(0)).collect();
-    return v.as_ptr();
+  let v: Vec<u16> = OsStr::new(value).encode_wide().chain(once(0)).collect();
+  return v.as_ptr();
 }
 
 // Window struct
 #[cfg(windows)]
 struct Window {
-    handle: HWND,
+  handle: HWND,
+}
+
+fn create_backbuffer(width: i32, height: i32) -> OffscreenBuffer {
+  unsafe {
+    let bytes_per_pixel = 4;
+    let bitmap_memory_size = width * height * bytes_per_pixel;
+    let buffer = OffscreenBuffer {
+      info: BITMAPINFO {
+        bmiHeader: BITMAPINFOHEADER {
+          biSize: mem::size_of::<BITMAPINFOHEADER>() as u32,
+          biWidth: width,
+          biHeight: height,
+          biPlanes: 1,
+          biBitCount: 32,
+          biCompression: 0,
+          biSizeImage: 0,
+          biXPelsPerMeter: 0,
+          biYPelsPerMeter: 0,
+          biClrUsed: 0,
+          biClrImportant: 0,
+        },
+        bmiColors: [RGBQUAD {
+          rgbBlue: 0,
+          rgbGreen: 0,
+          rgbRed: 0,
+          rgbReserved: 0,
+        }; 1],
+      },
+      width,
+      height,
+      pitch: width * bytes_per_pixel,
+      memory: VirtualAlloc(std::ptr::null_mut(), bitmap_memory_size as usize, MEM_COMMIT, PAGE_READWRITE),
+    };
+    return buffer;
+  }
 }
 
 fn hide_console_window() {
-    unsafe {
-        let window = GetConsoleWindow();
-        if window != std::ptr::null_mut() {
-            ShowWindow(window, SW_HIDE);
-        }
+  unsafe {
+    let window = GetConsoleWindow();
+    if window != std::ptr::null_mut() {
+      ShowWindow(window, SW_HIDE);
     }
+  }
+}
+
+fn render_gradient(back_buffer: &OffscreenBuffer, x_offset: i32, y_offset: i32) {
+//    let row = &back_buffer.memory as u8;
+  let row = &mut back_buffer.memory as *mut u8;
+  for y in 0..back_buffer.height {
+    let mut pixel: u32 = row as u32;
+    for x in 0..back_buffer.width {
+      let blue: u32 = ((x + x_offset) as u8).into();
+      let green: u32 = ((y + y_offset) as u8).into();
+      pixel = green << 8 | blue;
+    }
+  }
 }
 
 // window Proc
 pub unsafe extern "system" fn window_proc(hwnd: HWND,
                                           msg: UINT, w_param: WPARAM, l_param: LPARAM) -> LRESULT {
-    let hdc: HDC;
-    let lp_paint_struct: LPPAINTSTRUCT = libc::malloc(mem::size_of::<PAINTSTRUCT>() as libc::size_t) as *mut PAINTSTRUCT;
-    let lp_rect: LPRECT = libc::malloc(mem::size_of::<RECT>() as libc::size_t) as *mut RECT;
+  let hdc: HDC;
+  let lp_paint_struct: LPPAINTSTRUCT = libc::malloc(mem::size_of::<PAINTSTRUCT>() as libc::size_t) as *mut PAINTSTRUCT;
+  let lp_rect: LPRECT = libc::malloc(mem::size_of::<RECT>() as libc::size_t) as *mut RECT;
 
 
-    match msg {
-        WM_CREATE => {
-            println!("Created window")
-        }
+  match msg {
+    WM_CREATE => {
+      println!("Created window")
+    }
 //        WM_PAINT => {
 //            hdc = BeginPaint(hwnd, lp_paint_struct);
 //            GetClientRect(hwnd, lp_rect);
 //            DrawTextW(hdc, win32_string("Done with pride and prejudice by Culeva Alex"), -1, lp_rect, DT_SINGLELINE | DT_CENTER | DT_VCENTER);
 //            EndPaint(hwnd, lp_paint_struct);
 //        }
-        WM_DESTROY => {
-            println!("QUIT!");
-            PostQuitMessage(0);
-        }
-        _ => {
-            return DefWindowProcW(hwnd, msg, w_param, l_param);
-        }
+    WM_DESTROY => {
+      println!("QUIT!");
+      PostQuitMessage(0);
     }
+    _ => {
+      return DefWindowProcW(hwnd, msg, w_param, l_param);
+    }
+  }
 
-    return 0;
+  return 0;
 }
 
 // Create window function
 #[cfg(windows)]
 fn create_window(name: &str, title: &str) -> Result<Window, Error> {
-    let name = win32_string(name);
-    let title = win32_string(title);
+  let name = win32_string(name);
+  let title = win32_string(title);
 
-    unsafe {
+  unsafe {
 
-        // Create handle instance that will call GetModuleHandleW, which grabs the instance handle of WNDCLASSW (check third parameter)
-        let h_instance = GetModuleHandleW(null_mut());
+    // Create handle instance that will call GetModuleHandleW, which grabs the instance handle of WNDCLASSW (check third parameter)
+    let h_instance = GetModuleHandleW(null_mut());
 
-        // Create "class" for window, using WNDCLASSW struct (different from Window our struct)
-        let wnd_class = WNDCLASSW {
-            style: CS_OWNDC | CS_HREDRAW | CS_VREDRAW,        // Style
-            lpfnWndProc: Some(window_proc),            // The callbackfunction for any window event that can occur in our window!!! Here you could react to events like WM_SIZE or WM_QUIT.
-            hInstance: h_instance,                            // The instance handle for our application which we can retrieve by calling GetModuleHandleW.
-            lpszClassName: name,                    // Our class name which needs to be a UTF-16 string (defined earlier before unsafe). as_ptr() (Rust's own function) returns a raw pointer to the slice's buffer
-            cbClsExtra: 0,
-            cbWndExtra: 0,
-            hIcon: null_mut(),
-            hCursor: null_mut(),
-            hbrBackground: null_mut(),
-            lpszMenuName: null_mut(),
-        };
+    // Create "class" for window, using WNDCLASSW struct (different from Window our struct)
+    let wnd_class = WNDCLASSW {
+      style: CS_OWNDC | CS_HREDRAW | CS_VREDRAW,        // Style
+      lpfnWndProc: Some(window_proc),            // The callbackfunction for any window event that can occur in our window!!! Here you could react to events like WM_SIZE or WM_QUIT.
+      hInstance: h_instance,                            // The instance handle for our application which we can retrieve by calling GetModuleHandleW.
+      lpszClassName: name,                    // Our class name which needs to be a UTF-16 string (defined earlier before unsafe). as_ptr() (Rust's own function) returns a raw pointer to the slice's buffer
+      cbClsExtra: 0,
+      cbWndExtra: 0,
+      hIcon: null_mut(),
+      hCursor: null_mut(),
+      hbrBackground: null_mut(),
+      lpszMenuName: null_mut(),
+    };
 
-        // We have to register this class for Windows to use
-        RegisterClassW(&wnd_class);
+    // We have to register this class for Windows to use
+    RegisterClassW(&wnd_class);
 
-        // More info: https://msdn.microsoft.com/en-us/library/windows/desktop/ms632680(v=vs.85).aspx
-        // Create a window based on registered class
-        let handle = CreateWindowExW(
-            0,                                    // dwExStyle
-            name,                        // lpClassName, name of the class that we want to use for this window, which will be the same that we have registered before.
-            title,                        // lpWindowName
-            WS_OVERLAPPEDWINDOW | WS_VISIBLE,    // dwStyle
-            CW_USEDEFAULT,                        // Int x
-            CW_USEDEFAULT,                        // Int y
-            960,                        // Int nWidth
-            540,                        // Int nHeight
-            null_mut(),                            // hWndParent
-            null_mut(),                            // hMenu
-            h_instance,                            // hInstance
-            null_mut());                        // lpParam
+    // More info: https://msdn.microsoft.com/en-us/library/windows/desktop/ms632680(v=vs.85).aspx
+    // Create a window based on registered class
+    let handle = CreateWindowExW(
+      0,                                    // dwExStyle
+      name,                        // lpClassName, name of the class that we want to use for this window, which will be the same that we have registered before.
+      title,                        // lpWindowName
+      WS_OVERLAPPEDWINDOW | WS_VISIBLE,    // dwStyle
+      CW_USEDEFAULT,                        // Int x
+      CW_USEDEFAULT,                        // Int y
+      960,                        // Int nWidth
+      540,                        // Int nHeight
+      null_mut(),                            // hWndParent
+      null_mut(),                            // hMenu
+      h_instance,                            // hInstance
+      null_mut());                        // lpParam
 
-        if handle.is_null() {
-            Err(Error::last_os_error())
-        } else {
-            Ok(Window { handle })
-        }
+    if handle.is_null() {
+      Err(Error::last_os_error())
+    } else {
+      Ok(Window { handle })
     }
+  }
 }
 
 fn is_quit_message(msg: MSG) -> bool {
-    if msg.message == 161 {
-        return true;
-    }
-    false
+  if msg.message == 161 {
+    return true;
+  }
+  false
 }
 
 #[cfg(windows)]
@@ -193,81 +261,72 @@ fn is_quit_message(msg: MSG) -> bool {
 // More info: https://msdn.microsoft.com/en-us/library/windows/desktop/ms644927(v=vs.85).aspx
 // returns true if quit is called
 fn handle_messages(window: &mut Window) -> bool {
-    unsafe {
-        let mut message: MSG = mem::uninitialized();
+  unsafe {
+    let mut message: MSG = mem::uninitialized();
 
-        while PeekMessageW(&mut message as *mut MSG, window.handle, 0, 0, PM_REMOVE) > 0 {
-            if is_quit_message(message) { // QUIT
-                return true;
-            }
-            TranslateMessage(&message as *const MSG); // Translate message into something meaningful with TranslateMessage
-            DispatchMessageW(&message as *const MSG); // Dispatch message with DispatchMessageW
-        }
-        return false;
+    while PeekMessageW(&mut message as *mut MSG, window.handle, 0, 0, PM_REMOVE) > 0 {
+      if is_quit_message(message) { // QUIT
+        return true;
+      }
+      TranslateMessage(&message as *const MSG); // Translate message into something meaningful with TranslateMessage
+      DispatchMessageW(&message as *const MSG); // Dispatch message with DispatchMessageW
     }
+    return false;
+  }
 }
 
 fn get_input(game_state: &mut GameState) {
-    unsafe {
-        game_state.input.quit_key = GetAsyncKeyState(VK_ESCAPE) != 0;
-        game_state.input.left_key = GetAsyncKeyState(0x41) != 0;
-        game_state.input.right_key = GetAsyncKeyState(0x44) != 0;
-        game_state.input.down_key = GetAsyncKeyState(0x53) != 0;
-        game_state.input.up_key = GetAsyncKeyState(0x57) != 0;
-    }
+  unsafe {
+    game_state.input.quit_key = GetAsyncKeyState(VK_ESCAPE) != 0;
+    game_state.input.left_key = GetAsyncKeyState(0x41) != 0;
+    game_state.input.right_key = GetAsyncKeyState(0x44) != 0;
+    game_state.input.down_key = GetAsyncKeyState(0x53) != 0;
+    game_state.input.up_key = GetAsyncKeyState(0x57) != 0;
+  }
 }
 
-fn render(game_state: &mut GameState, window: &mut Window) {
-    unsafe {
-        let hdc: HDC;
-        let lp_paint_struct: LPPAINTSTRUCT = libc::malloc(mem::size_of::<PAINTSTRUCT>() as libc::size_t) as *mut PAINTSTRUCT;
-        let lp_rect: LPRECT = libc::malloc(mem::size_of::<RECT>() as libc::size_t) as *mut RECT;
-        hdc = BeginPaint(window.handle, lp_paint_struct);
-        GetClientRect(window.handle, lp_rect);
-        DrawTextW(hdc, win32_string("Done with pride and prejudice by Culeva Alex!!!"), -1, lp_rect, DT_SINGLELINE | DT_CENTER | DT_VCENTER);
-        EndPaint(window.handle, lp_paint_struct);
-    }
-}
+fn render(game_state: &mut GameState, window: &mut Window, back_buffer: &OffscreenBuffer) {}
 
-fn main_loop(window: &mut Window, game_state: &mut GameState) -> bool {
-    if handle_messages(window) {
-        return true;
-    }
+fn main_loop(window: &mut Window, game_state: &mut GameState, back_buffer: &OffscreenBuffer) -> bool {
+  if handle_messages(window) {
+    return true;
+  }
 
-    game_state.frame += 1;
-    game_state.time.last_frame_time = game_state.time.frame_start_time.elapsed();
-    game_state.time.frame_start_time = Instant::now();
+  game_state.frame += 1;
+  game_state.time.last_frame_time = game_state.time.frame_start_time.elapsed();
+  game_state.time.frame_start_time = Instant::now();
 
-    get_input(game_state);
+  get_input(game_state);
 
 
-    let continue_running = game_loop(game_state);
-    if !continue_running {
-        return true;
-    }
+  let continue_running = game_loop(game_state);
+  if !continue_running {
+    return true;
+  }
 
-    println!("{:?}", game_state.player);
-    render(game_state, window);
-    let frame_time = game_state.time.frame_start_time.elapsed();
-    if frame_time < Duration::from_millis(15) {
-        let sleep_time = Duration::from_millis((15 - frame_time.subsec_millis()).into());
-        //println!("Sleeping for {:?}", sleep_time);
-        std::thread::sleep(sleep_time);
-    }
+  println!("{:?}", game_state.player);
+  render(game_state, window, back_buffer);
+  let frame_time = game_state.time.frame_start_time.elapsed();
+  if frame_time < Duration::from_millis(15) {
+    let sleep_time = Duration::from_millis((15 - frame_time.subsec_millis()).into());
+    //println!("Sleeping for {:?}", sleep_time);
+    std::thread::sleep(sleep_time);
+  }
 
-    return false;
+  return false;
 }
 
 
 #[cfg(windows)]
 fn main() {
-    hide_console_window();
+  hide_console_window();
 
-    let mut window = create_window("my_window", "Portfolio manager pro").unwrap();
-    let mut game_state = GameState::new();
-    loop {
-        if main_loop(&mut window, &mut game_state) {
-            break;
-        }
+  let mut window = create_window("my_window", "Portfolio manager pro").unwrap();
+  let back_buffer = create_backbuffer(960, 540);
+  let mut game_state = GameState::new();
+  loop {
+    if main_loop(&mut window, &mut game_state, &back_buffer) {
+      break;
     }
+  }
 }
