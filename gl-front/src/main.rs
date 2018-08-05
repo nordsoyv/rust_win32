bb396ca5bbd4e496b11d8d9136f348385cd14bfc
Øyvind <oyvind.nordsveen@gmail.com>
2018-08-05 16:56:38 +0200

    gl tut part 3

--- gl-front/src/main.rs
+++ gl-front/src/main.rs
@@ -3,8 +3,8 @@ extern crate glutin;
 
 use glutin::dpi::*;
 use glutin::GlContext;
-use std::ffi::CString;
-use std::ffi::CStr;
+
+mod render_gl;
 
 fn main() {
     let mut events_loop = glutin::EventsLoop::new();
@@ -29,6 +29,22 @@ fn main() {
     }
 
     let mut running = true;
+
+    use std::ffi::CString;
+
+    let vert_shader = render_gl::Shader::from_vert_source(
+        &CString::new(include_str!("triangle.vert")).unwrap()
+    ).unwrap();
+
+    let frag_shader = render_gl::Shader::from_frag_source(
+        &CString::new(include_str!("triangle.frag")).unwrap()
+    ).unwrap();
+
+    let shader_program = render_gl::Program::from_shaders(
+        &[vert_shader, frag_shader]
+    ).unwrap();
+    shader_program.set_used();
+
     while running {
         events_loop.poll_events(|event| {
             match event {
@@ -50,73 +66,4 @@ fn main() {
 
         gl_window.swap_buffers().unwrap();
     }
-}
-
-struct Shader {
-    id : gl::types::GLuint,
-}
-
-impl Shader {
-    fn from_source( source : &CStr, kind : gl::types::GLuint ) -> Result<Shader, String > {
-        let id = shader_from_source(source, kind)?;
-        Ok(Shader{id})
-    }
-
-    fn from_vert_source(source: &CStr) -> Result<Shader, String> {
-        Shader::from_source(source, gl::VERTEX_SHADER)
-    }
-
-    fn from_frag_source(source: &CStr) -> Result<Shader, String> {
-        Shader::from_source(source, gl::FRAGMENT_SHADER)
-    }
-}
-
-impl Drop for Shader {
-    fn drop(&mut self){
-        unsafe {
-            gl::DeleteShader(self.id);
-        }
-    }
-}
-
-fn shader_from_source(source: &CStr, kind: gl::types::GLuint) -> Result<gl::types::GLuint, String> {
-    let id = unsafe { gl::CreateShader(kind) };
-
-    unsafe {
-        gl::ShaderSource(id, 1 , source.as_ptr(), std::ptr::null());
-        gl::CompileShader(id);
-    }
-
-    let mut success: gl::types::GLint = 1;
-    unsafe {
-        gl::GetShaderiv(id, gl::COMPILE_STATUS, &mut success);
-    }
-
-    if success == 0 {
-        let mut len: gl::types::GLint = 0;
-        unsafe {
-            gl::GetShaderiv(id, gl::INFO_LOG_LENGTH, &mut len);
-        }
-        let error = create_whitespace_cstring_with_len(len as usize);
-        unsafe {
-            gl::GetShaderInfoLog(
-                id,
-                len,
-                std::ptr::null_mut(),
-                error.as_ptr() as *mut gl::types::GLchar,
-            );
-        }
-
-        return Err(error.to_string_lossy().into_owned());
-    }
-    Ok(id)
-}
-
-fn create_whitespace_cstring_with_len(len: usize) -> CString {
-    // allocate buffer of correct size
-    let mut buffer: Vec<u8> = Vec::with_capacity(len + 1);
-    // fill it with len spaces
-    buffer.extend([b' '].iter().cycle().take(len));
-    // convert buffer to CString
-    unsafe { CString::from_vec_unchecked(buffer) }
 }
