493f44ae97948d1885f67ee3780041fb85bacbd0
Øyvind Nordsveen <oyvind.nordsveen@confirmit.com>
2018-08-10 10:41:27 +0200

    Autogenerated code works

--- gl-front/src/main.rs
+++ gl-front/src/main.rs
@@ -3,41 +3,45 @@ extern crate gl;
 extern crate glutin;
 
 #[macro_use]
+extern crate game_derive;
+
+#[macro_use]
 extern crate failure;
 
 mod render_gl;
 mod resources;
 
-use failure::err_msg;
 use glutin::{dpi::*, GlContext};
 use render_gl::data;
 use resources::Resources;
 use std::path::Path;
 
-#[derive(Copy, Clone, Debug)]
+#[derive(VertexAttribPointers, Copy, Clone, Debug)]
 #[repr(C, packed)]
 struct Vertex {
+    #[location = "0"]
     pos: data::f32_f32_f32,
+    #[location = "1"]
     clr: data::f32_f32_f32,
 }
 
-impl Vertex {
-    fn vertex_attrib_pointers(gl: &gl::Gl) {
-        let stride = std::mem::size_of::<Self,>();
-        let location = 0;
-        let offset = 0;
-
-        unsafe {
-            data::f32_f32_f32::vertex_attrib_pointer(gl, stride, location, offset,);
-        }
-        let location = 1;
-        let offset = offset + std::mem::size_of::<data::f32_f32_f32,>();
-
-        unsafe {
-            data::f32_f32_f32::vertex_attrib_pointer(gl, stride, location, offset,);
-        }
-    }
-}
+//impl Vertex {
+//    fn vertex_attrib_pointers(gl: &gl::Gl) {
+//        let stride = std::mem::size_of::<Self,>();
+//        let location = 0;
+//        let offset = 0;
+//
+//        unsafe {
+//            data::f32_f32_f32::vertex_attrib_pointer(gl, stride, location, offset,);
+//        }
+//        let location = 1;
+//        let offset = offset + std::mem::size_of::<data::f32_f32_f32,>();
+//
+//        unsafe {
+//            data::f32_f32_f32::vertex_attrib_pointer(gl, stride, location, offset,);
+//        }
+//    }
+//}
 
 fn main() {
     let res = run();
